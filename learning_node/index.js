1ee927f87107264e4fb8e1280dc9cac5b7d299be
Mehmet Konukcu <mehmetext68@gmail.com>
2022-07-12 11:52:38 +0300

    modül kullanımı devamı

--- learning_node/index.js
+++ learning_node/index.js
@@ -1,5 +1,10 @@
-import { hello, topla } from "./my_module.js";
+import hello, { cikar, topla, text, user, cities } from "./my_module.js";
 
 console.log(topla(2, 3));
+console.log(cikar(2, 4));
 
-console.log(hello("Mehmet"));
+hello(text);
+
+console.log(user);
+
+console.log(cities);
--- learning_node/my_module.js
+++ learning_node/my_module.js
@@ -1,7 +1,20 @@
 const hello = (name) => {
-	console.log("hello " + name);
+	// console.log("hello " + name);
+	console.log(`Hello, ${name}!`);
 };
 
 const topla = (a, b) => a + b;
+const cikar = (a, b) => a - b;
 
-export { hello, topla };
+export const text = "Mehmet";
+
+export const user = {
+	id: 1,
+	name: "Mehmet",
+};
+
+export const cities = ["Aksaray", "Konya", "Ankara"];
+
+export { topla, cikar };
+
+export default hello;
