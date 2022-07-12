4c315c0778217d598912403f8824ba6c47bd16c1
Mehmet Konukcu <mehmetext68@gmail.com>
2022-07-13 01:18:22 +0300

    first-react-app oluşturuldu

---
+++ first-react-app/src/App.js
@@ -0,0 +1,18 @@
+function App() {
+	return (
+		<div className="App">
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					height: "100vh",
+				}}
+			>
+				<h1>Hello, React!</h1>
+			</div>
+		</div>
+	);
+}
+
+export default App;
