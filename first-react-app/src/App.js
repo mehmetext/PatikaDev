c2aae657ddc97d3a7a5ffe31dd47ae0408ec9910
Mehmet Konukcu <mehmetext68@gmail.com>
2022-07-13 16:25:26 +0300

    jsx ve temel kurallar

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -6,6 +6,16 @@ function App() {
 		<div className="App">
 			<h1>Hello React</h1>
 			<Header />
+			<p className="xyz">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+				consectetur metus turpis, at imperdiet leo suscipit sed. Etiam
+				quis diam a est congue auctor quis et odio.
+			</p>
+
+			<label htmlFor="myInput">
+				Name:
+				<input id="myInput" />
+			</label>
 		</div>
 	);
 }
