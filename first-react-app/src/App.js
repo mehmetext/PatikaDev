8b04b8f00972efcd79f21b83395be3f8e2632070
Mehmet Konukcu <mehmetext68@gmail.com>
2022-07-13 16:50:36 +0300

    koşullu render işlemi

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,13 +3,27 @@ import Header from "./components/Header";
 
 const name = "Mehmet";
 const surname = "Konukçu";
+const isLoggedIn = true;
 
 function App() {
 	return (
 		<>
 			<h1>
-				{name} {surname}
+				{isLoggedIn && (
+					<>
+						<Header />
+						{name} {surname}
+					</>
+				)}
 			</h1>
+
+			{!isLoggedIn && (
+				<>
+					Kullanıcı Yok <br />
+				</>
+			)}
+
+			{isLoggedIn ? "Çıkış yap" : "Giriş Yap"}
 		</>
 	);
 }
