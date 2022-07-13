536084e43084fa891273039dbdaf2dc03890115c
Mehmet Konukcu <mehmetext68@gmail.com>
2022-07-13 20:32:47 +0300

    Array States kullanımı

--- react-states/src/App.js
+++ react-states/src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 function App() {
 	const [name, setName] = useState("Mehmet");
 	const [age, setAge] = useState(21);
+	const [friends, setFriends] = useState(["Ahmet", "Murat"]);
 
 	return (
 		<div className="App">
@@ -10,6 +11,20 @@ function App() {
 			<h2>{age}</h2>
 			<button onClick={() => setName("Ahmet")}>Change Name</button>
 			<button onClick={() => setAge(25)}>Change Age</button>
+
+			<hr />
+			{friends && (
+				<>
+					<h1>Friends</h1>
+					{friends.map((friend, i) => (
+						<div key={i}>{friend}</div>
+					))}
+				</>
+			)}
+
+			<button onClick={() => setFriends([...friends, "Ayşe"])}>
+				Add Friend
+			</button>
 		</div>
 	);
 }
