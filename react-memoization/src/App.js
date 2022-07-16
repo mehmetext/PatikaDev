24fde64065ded605bf43d6a6d75ee4214a42afb6
Mehmet Konukcu <mehmetext68@gmail.com>
2022-07-17 00:30:06 +0300

    useMemo kullanımı

--- react-memoization/src/App.js
+++ react-memoization/src/App.js
@@ -1,19 +1,35 @@
 import "./styles.css";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import Header from "./components/Header";
 
 function App() {
 	const [number, setNumber] = useState(0);
+	const [text, setText] = useState("");
+
+	const data = useMemo(() => {
+		return calculateData(number);
+	}, [number]);
 
 	return (
 		<div className="App">
-			<Header />
+			<Header data={data} />
 			<h1>{number}</h1>
 			<div>
 				<button onClick={() => setNumber(number + 1)}>Arttır</button>
 			</div>
+			<br />
+			<div>
+				<input value={text} onChange={(e) => setText(e.target.value)} />
+			</div>
 		</div>
 	);
+}
+
+function calculateData(number) {
+	console.log("calculating");
+	for (let i = 0; i < 1000000000; i++) {}
+	console.log("calculating ended");
+	return { name: "Mehmet", number };
 }
 
 export default App;
--- react-memoization/src/components/Header.js
+++ react-memoization/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Header() {
+function Header({ data }) {
 	console.log("Header render edildi");
 	return (
 		<>
 			<div>
 				<h1>HEADER</h1>
 			</div>
+			<pre>{JSON.stringify(data, null, 2)}</pre>
 			<hr />
 		</>
 	);
